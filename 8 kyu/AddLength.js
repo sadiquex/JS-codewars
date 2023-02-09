0beca289a61ef128bf33270bd5b8ddb3aab10971
sadiquex <abubakasaddik1@gmail.com>
2023-02-09 11:44:44 +0000

    Solved 3 problems

---
+++ 8 kyu/AddLength.js
@@ -0,0 +1,12 @@
+/* What if we need the length of the words separated by a space to be 
+added at the end of that same word and have it returned as an array?
+
+Example(Input --> Output)
+"apple ban" --> ["apple 5", "ban 3"]
+"you will win" -->["you 3", "will 4", "win 3"] */
+
+function addLength(str) {
+  return str.split(" ").map((el) => `${el} ${el.length}`);
+}
+
+console.log(addLength("apple ban"));
