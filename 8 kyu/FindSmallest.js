852d26bbf66b3718195f9e6a9b76ea7bf037270d
sadiquex <abubakasaddik1@gmail.com>
2023-02-14 20:42:56 +0000

    Smallest from array

---
+++ 8 kyu/FindSmallest.js
@@ -0,0 +1,21 @@
+/* Given an array of integers your solution should find the smallest integer.
+
+For example:
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty. */
+
+class SmallestIntegerFinder {
+  //         function findSmallestInt (nums) {
+  //   let smallest = nums[0];
+  //   for (i = 0; i < nums.length; i++) {
+  //     if (nums[i] < smallest) {
+  //       return (smallest = nums[i]);
+  //     }
+  //   }
+  // };
+  // console.log(234);
+}
+
+const res = new SmallestIntegerFinder();
+console.log(res);
