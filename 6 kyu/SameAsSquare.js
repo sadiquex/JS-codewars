a964b20d154619f530080d3e1b5373dbbd407603
sadiquex <abubakasaddik1@gmail.com>
2023-02-23 07:56:06 +0000

    Same as square

---
+++ 6 kyu/SameAsSquare.js
@@ -0,0 +1,34 @@
+/*
+Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+Examples
+Valid arrays
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11] 
+b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+
+*/
+
+function comp(array1, array2) {
+  return (
+    array1
+      .map((el) => el ** 2)
+      // for every element in array2, check if array2 includes that element
+      .every((element) => array2.includes(element))
+  );
+}
+
+console.log(
+  comp(
+    [121, 144, 19, 161, 19, 144, 19, 11],
+    [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+  )
+);
+
+// square all the elements in array 1
+// check for the elements in array 2
+// compare the two arrays
+// check all the elements in array 2
