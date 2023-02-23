4ca422f76e72a610be349e87988dbf420fecccca
sadiquex <abubakasaddik1@gmail.com>
2023-02-23 08:26:26 +0000

    Refactored code

--- 6 kyu/SameAsSquare.js
+++ 6 kyu/SameAsSquare.js
@@ -12,23 +12,36 @@ b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
 
 */
 
-function comp(array1, array2) {
+function compare(array1, array2) {
+  if (array1 == null || array2 == null) return false;
+  // sort arrays in ascending order
+  array1.sort((a, b) => a - b);
+  array2.sort((a, b) => a - b);
+
   return (
     array1
       .map((el) => el ** 2)
-      // for every element in array2, check if array2 includes that element
-      .every((element) => array2.includes(element))
+      // for the element in the current iteration in array1,
+      // return it if it is the current iteration in array2.
+
+      // the every method is running on array1
+      // the every method's parameters are a function and an array
+      // the function takes currentValue,index
+      .every((element, i) => element == array2[i])
+    // for every element in array1, return element if element is equal to the
+    // current iteration in array2
   );
 }
 
 console.log(
-  comp(
+  compare(
     [121, 144, 19, 161, 19, 144, 19, 11],
     [121, 14641, 20736, 361, 25921, 361, 20736, 361]
   )
 );
 
-// square all the elements in array 1
-// check for the elements in array 2
-// compare the two arrays
-// check all the elements in array 2
+// check if the array is null
+// sort both arrays
+// square the elements in the first array
+// compare the elements in the iteration,since theyre sorted the same
+// if all the elements are the same, return them, else do not return.
