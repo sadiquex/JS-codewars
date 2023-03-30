098d24dc2368178afe33b46e07821e7d06609c6b
sadiquex <abubakasaddik1@gmail.com>
2023-03-30 22:51:37 +0100

    Make Negative

---
+++ 8 kyu/makeNegative.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+function makeNegative(num) {
+  if (num < 0) {
+    return num * -1;
+  } else if (num > 1) {
+    return num * -1;
+  }
+
+  //   if num > 0 then return negative, else return positive
+}
+
+console.log(makeNegative(5));
